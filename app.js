349cf3f4d0356eeceff389d1d9c449d603604f39
neilxcode <developros@icloud.com>
2018-05-06 17:44:43 -0400

    Create Favorites list box on home page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+// const User = require("../models/userSchema");
 
 
 mongoose.Promise = Promise;
@@ -48,6 +49,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
+
+
 
 
 
