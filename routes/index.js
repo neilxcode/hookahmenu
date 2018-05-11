ae65fcb69db7968dcde7de3c0c06a5be6d1812b7
neilxcode <developros@icloud.com>
2018-05-11 10:31:13 -0400

    commented out index path duplicate

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const mongoose     = require('mongoose');
 
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-      res.render("index");
-});
+// router.get('/', (req, res, next) => {
+//       res.render("index");
+// });
 
 module.exports = router;
